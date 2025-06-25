9d4178666a458d9ec06bf8f4e64223eb94f3a2c8
Ansh Bhutani <bhutaniansh07@gmail.com>
2025-06-25 12:44:55 +0530

    4.js

---
+++ 4.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Thank you for your message! We’ll get back to you soon ✨");
+      form.reset();
+    });
+  }
+});
